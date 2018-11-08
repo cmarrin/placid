24b822bd6f09f5b0423e7c3006269c3fa2310150
Chris Marrin <chris@marrin.com>
2018-11-08 07:41:18 -0800

    Minor change

--- mac/ESPWifi/ESPWifi.ino
+++ mac/ESPWifi/ESPWifi.ino
@@ -33,8 +33,6 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 -------------------------------------------------------------------------*/
 
-// This file is shared between assembly and c/c++
-
 #pragma once
 
 // Ports
