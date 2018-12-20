53bc49a07a21dd860f93fa7958e5ea95924c0947
Chris Marrin <chris@marrin.com>
2018-12-20 10:53:51 -0800

    Added Mutex class for later

---
+++ baremetal/Darwin/DarwinMutex.cpp
@@ -0,0 +1,9 @@
+//
+//  Mutex.cpp
+//  kerneltest
+//
+//  Created by Chris Marrin on 12/12/18.
+//  Copyright © 2018 MarrinTech. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ baremetal/bare/Mutex.h
@@ -0,0 +1,13 @@
+//
+//  Mutex.h
+//  placid
+//
+//  Created by Chris Marrin on 12/12/18.
+//  Copyright © 2018 MarrinTech. All rights reserved.
+//
+
+#ifndef Mutex_h
+#define Mutex_h
+
+
+#endif /* Mutex_h */
