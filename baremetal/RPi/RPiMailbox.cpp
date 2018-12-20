843e24eb4a636d427ebaef1d9dbfc9ae14268705
Chris Marrin <chris@marrin.com>
2018-12-20 11:01:01 -0800

    Improvements to Mailbox interface for RPi

--- baremetal/RPi/RPiMailbox.cpp
+++ baremetal/RPi/RPiMailbox.cpp
@@ -35,9 +35,10 @@ POSSIBILITY OF SUCH DAMAGE.
 
 #include "bare.h"
 
-#include "bare/Mailbox.h"
+#include "RPiMailbox.h"
 
 #include "bare/Serial.h"
+#include <cassert>
 
 using namespace bare;
 
@@ -46,12 +47,18 @@ static volatile unsigned int *MAILBOX0STATUS = (unsigned int *) 0x2000b898;
 static volatile unsigned int *MAILBOX0WRITE = (unsigned int *) 0x2000b8a0;
 
 static constexpr uint32_t MailboxResponse = 0x80000000;
+static constexpr uint32_t GPUAddressAlias = 0x40000000;
 
 #define MAILBOX_FULL 0x80000000
 #define MAILBOX_EMPTY 0x40000000
 
 static inline void dmb() { __asm volatile ("mcr p15, #0, %[zero], c7, c10, #5" : : [zero] "r" (0) ); }
 static inline void flushcache() { __asm volatile ("mcr p15, #0, %[zero], c7, c14, #0" : : [zero] "r" (0) ); }
+
+static inline uint32_t ARMaddrToGPUaddr(void* addr)
+{
+    return reinterpret_cast<uint32_t>(addr) | GPUAddressAlias;
+}
 
 // This code is from corn-mainline by Raspberry Alpha Omega
 
@@ -85,9 +92,12 @@ static uint32_t readMailbox(Mailbox::Channel channel)
     return 0;
 }
 
-static void writeMailbox(Mailbox::Channel channel, uint32_t data)
+static void writeMailbox(Mailbox::Channel channel, uint32_t* addr)
 {
-    assert((data & 0xf) == 0);
+    // Addr must be 16 byte aligned
+    assert((reinterpret_cast<uintptr_t>(addr) & 0xf) == 0);
+    
+    uint32_t data = ARMaddrToGPUaddr(addr);
 
     // Wait for mailbox to be not full
     while (*MAILBOX0STATUS & MAILBOX_FULL)     {
@@ -99,7 +109,7 @@ static void writeMailbox(Mailbox::Channel channel, uint32_t data)
     *MAILBOX0WRITE = (data | static_cast<uint32_t>(channel));
 }
 
-Mailbox::Error Mailbox::getParameter(Param param, uint32_t* result, uint32_t size)
+Mailbox::Error Mailbox::getParameter(Command cmd, uint32_t* result, uint32_t size)
 {
     static constexpr uint32_t BufferPadding = 6;
     static constexpr uint32_t MaxSize = 256 - BufferPadding;
@@ -112,13 +122,12 @@ Mailbox::Error Mailbox::getParameter(Param param, uint32_t* result, uint32_t siz
 
     buf[0] = (size + BufferPadding) * sizeof(uint32_t);
     buf[1] = 0; // Request code: process request
-    buf[2] = static_cast<uint32_t>(param);
+    buf[2] = static_cast<uint32_t>(cmd);
     buf[3] = size * sizeof(uint32_t); // value buffer size in bytes ((1 entry)*4)
     buf[4] = 0; // 1bit request/response (0/1), 31bit value length in bytes
 
-    uint32_t addr = static_cast<uint32_t>((reinterpret_cast<uintptr_t>(buf)) + 0x40000000);
-    writemailbox(Channel::Tags, addr);
-    uint32_t mail = readmailbox(8);
+    writeMailbox(Channel::Tags, buf);
+    uint32_t mail = readMailbox(Channel::Tags);
     dmb();
     
     uint32_t* responseBuf = reinterpret_cast<uint32_t*>(mail);
@@ -132,25 +141,25 @@ Mailbox::Error Mailbox::getParameter(Param param, uint32_t* result, uint32_t siz
 void Mailbox::printBoardParams()
 {
     uint32_t responseBuf[2];
-    getParameter(Mailbox::Param::FirmwareRev, responseBuf, 1);
+    getParameter(Mailbox::Command::GetFirmwareRev, responseBuf, 1);
     Serial::printf("FirmwareRev: %d\n", responseBuf[0]);
     
-    getParameter(Mailbox::Param::BoardModel, responseBuf, 1);
+    getParameter(Mailbox::Command::GetBoardModel, responseBuf, 1);
     Serial::printf("BoardModel: %d\n", responseBuf[0]);
     
-    getParameter(Mailbox::Param::BoardRev, responseBuf, 1);
+    getParameter(Mailbox::Command::GetBoardRev, responseBuf, 1);
     Serial::printf("BoardRev: %d\n", responseBuf[0]);
     
-    getParameter(Mailbox::Param::BoardSerialNo, responseBuf, 2);
+    getParameter(Mailbox::Command::GetBoardSerialNo, responseBuf, 2);
     Serial::printf("BoardSerialNo: %d, %d\n", responseBuf[0], responseBuf[1]);
     
-    getParameter(Mailbox::Param::ARMMemory, responseBuf, 2);
+    getParameter(Mailbox::Command::GetARMMemory, responseBuf, 2);
     Serial::printf("ARMMemory: start=0x%08x, size=0x%08x\n", responseBuf[0], responseBuf[1]);
     
-    getParameter(Mailbox::Param::VCMemory, responseBuf, 2);
+    getParameter(Mailbox::Command::GetVCMemory, responseBuf, 2);
     Serial::printf("VCMemory: start=0x%08x, size=0x%08x\n", responseBuf[0], responseBuf[1]);
     
-    getParameter(Mailbox::Param::DMAChannelMask, responseBuf, 1);
+    getParameter(Mailbox::Command::GetDMAChannels, responseBuf, 1);
     Serial::printf("DMAChannelMask: 0x%08x\n", responseBuf[0]);
 }
 
@@ -176,7 +185,7 @@ Mailbox::Error Mailbox::tagMessage(uint32_t* responseBuf, uint8_t size, ...)
     
     va_end(list);
     
-    writeMailbox(Channel::Tags, ARMaddrToGPUaddr(&message[0]));
+    writeMailbox(Channel::Tags, message);
     
     // Wait for response
     readMailbox(Channel::Tags);
@@ -187,8 +196,8 @@ Mailbox::Error Mailbox::tagMessage(uint32_t* responseBuf, uint8_t size, ...)
                 responseBuf[i] = message[2 + i];
             }
         }
-        return true;
+        return Error::OK;
     }
-    return false;
+    return Error::InvalidResponse;
 
 }
--- baremetal/RPi/RPiMailbox.h
+++ baremetal/RPi/RPiMailbox.h
@@ -45,6 +45,7 @@ namespace bare {
         enum class Error {
             OK,
             SizeTooLarge,
+            InvalidResponse,
         };
         
         enum class Channel {
@@ -61,17 +62,121 @@ namespace bare {
         };
 
         enum class Command {
-            FirmwareRev = 0x00000001,   // uint32_t rev
-            BoardModel = 0x00010001,    // uint32_t model
-            BoardRev = 0x00010002,      // uint32_t boardRev
-            MACAddress = 0x00010003,    // uint8_t addr[6]
-            BoardSerialNo = 0x00010004, // uint32_t ser[2]
-            ARMMemory = 0x00010005,     // uint32_t base, uint32_t size
-            VCMemory = 0x00010006,      // uint32_t base, uint32_t size
-            DMAChannelMask = 0x00060001,// uint32_t mask
+            // Videocore info commands
+            GetFirmwareRev          = 0x00000001,   // < uint32_t rev
+            GetBoardModel           = 0x00010001,   // < uint32_t model
+            GetBoardRev             = 0x00010002,   // < uint32_t boardRev
+            GetMACAddress           = 0x00010003,   // < uint8_t addr[6]
+            GetBoardSerialNo        = 0x00010004,   // < uint32_t ser[2]
+            GetARMMemory            = 0x00010005,   // < uint32_t base, uint32_t size
+            GetVCMemory             = 0x00010006,   // < uint32_t base, uint32_t size
+            GetClocks               = 0x00010007,   // < { uint32_t parentId, uint32_t id }+
+
+            // Power commands
+            GetPowerState           = 0x00020001,   // 
+            GetTiming               = 0x00020002,   // 
+            SetPowerState           = 0x00028001,   // 
+
+            // GPIO commands
+            GetGPIOState            = 0x00030041,   // 
+            SetGPIOState            = 0x00038041,   // 
+
+            // Clock commands
+            GetClockState           = 0x00030001,   // 
+            GetClockRate            = 0x00030002,   // 
+            GetMaxClockRate         = 0x00030004,   // 
+            GetMinClockRate         = 0x00030007,   // 
+            GetTurbo                = 0x00030009,   // 
+
+            SetClockState           = 0x00038001,   // 
+            SetClockRate            = 0x00038002,   // 
+            SetTurbo                = 0x00038009,   // 
+
+            // Voltage commands
+            GetVoltage              = 0x00030003,   // 
+            GetMaxVoltage           = 0x00030005,   // 
+            GetMinVoltage           = 0x00030008,   // 
+
+            SetVoltage              = 0x00038003,   // 
+
+            // Temperature commands
+            GetTemperature          = 0x00030006,   // 
+            GetMaxTemperature       = 0x0003000A,   // 
+
+            // Memory commands
+            AllocateMemory          = 0x0003000C,   // 
+            LockMemory              = 0x0003000D,   // 
+            UnlockMemory            = 0x0003000E,   // 
+            ReleaseMemory           = 0x0003000F,   // 
+                                                                   
+            // Execute code commands
+            ExecuteCode             = 0x00030010,   // 
+
+            // QPU control commands
+            ExecuteQPU              = 0x00030011,   // 
+            EnableQPU               = 0x00030012,   // 
+
+            // Displaymax commands
+            GetDispmanxHandle       = 0x00030014,   // 
+            GetEDIDBlock            = 0x00030020,   // 
+
+            // SD Card commands
+            GetSDHostClock          = 0x00030042,   // 
+            SetSDHostClock          = 0x00038042,   // 
+
+            // Framebuffer commands
+            AllocateFrameBuffer     = 0x00040001,   // 
+            BlankScreen             = 0x00040002,   // 
+            GetPhysicalWidthHeight  = 0x00040003,   // 
+            GetVirtualWidthHeight   = 0x00040004,   // 
+            GetColorDepth           = 0x00040005,   // 
+            GetPixelOrder           = 0x00040006,   // 
+            GetAlphaMode            = 0x00040007,   // 
+            GetPitch                = 0x00040008,   // 
+            GetVirtualOffset        = 0x00040009,   // 
+            GetOverscan             = 0x0004000A,   // 
+            GetPalette              = 0x0004000B,   // 
+
+            ReleaseFramebuffer      = 0x00048001,   // 
+            SetPhysicalWidthHeight  = 0x00048003,   // 
+            SetVirtualWidthHeight   = 0x00048004,   // 
+            SetColorDepth           = 0x00048005,   // 
+            SetPixelOrder           = 0x00048006,   // 
+            SetAlphaMode            = 0x00048007,   // 
+            SetVirtualOffset        = 0x00048009,   // 
+            SetOverscan             = 0x0004800A,   // 
+            SetPalette              = 0x0004800B,   // 
+            SetVSync                = 0x0004800E,   // 
+            SetBacklight            = 0x0004800F,   // 
+
+            // VCHIQ commands
+            VCHIQInit               = 0x00048010,   // 
+
+            // Config commands
+            GetCommandLine          = 0x00050001,   //  
+
+            // Shared resource management commands
+            GetDMAChannels          = 0x00060001,   // uint32_t mask
+
+            /* Cursor commands */
+            SetCursorInfo           = 0x00008010,   // 
+            SetCursorState          = 0x00008011,   // 
         };
         
-        static Error getParameter(Param, uint32_t* result, uint32_t size);
+        enum class ClockId {
+            EMMC    = 0x1, 
+            UART    = 0x2,
+            ARM     = 0x3,
+            Core    = 0x4,
+            V3D     = 0x5,
+            H264    = 0x6,
+            ISP     = 0x7,
+            SDRAM   = 0x8,
+            Pixel   = 0x9,
+            PWM     = 0xA,
+        };
+        
+        static Error getParameter(Command, uint32_t* result, uint32_t size);
         static Error tagMessage(uint32_t* responseBuf, uint8_t size, ...);
         static void printBoardParams();
     };
