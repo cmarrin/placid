d827ed215c8ca3982ce890a8cb01c4e16557bc86
Chris Marrin <cmarrin@apple.com>
2019-01-14 10:02:24 -0800

    Added Singleton template

---
+++ baremetal/bare/Singleton.h
@@ -0,0 +1,13 @@
+//
+//  Singleton.h
+//  placid
+//
+//  Created by Chris Marrin on 1/14/19.
+//  Copyright © 2019 MarrinTech. All rights reserved.
+//
+
+#ifndef Singleton_h
+#define Singleton_h
+
+
+#endif /* Singleton_h */
